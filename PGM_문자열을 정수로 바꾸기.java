459670a9b3772e56c78388decefebdc345bb91de
Kim-Ha-neul <68495701+Kim-Ha-neul@users.noreply.github.com>
2021-05-26 14:10:21 +0900

    feat : PGM 문자열을 정수로 바꾸기

---
+++ PGM_문자열을 정수로 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
+        	answer = Integer.parseInt(s.substring(1, s.length()));
+            if(s.charAt(0) == '-')
+            	answer *= -1;
+        } else
+        	answer = Integer.parseInt(s);
+  
+        return answer;
+    }
+}
