c01b16f7aa0f768615d6058d254dd7227fc1b056
Kim-Ha-neul <68495701+Kim-Ha-neul@users.noreply.github.com>
2021-06-16 12:33:37 +0900

    feat : PGM 이상한 문자 만들기

---
+++ PGM_이상한 문자 만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        char[] c = new char[s.length()];
+        c = s.toCharArray();
+        int index = 0;
+        for(int i=0; i<s.length(); i++){
+            if(c[i] == ' '){
+                index = 0;
+                answer += " ";
+            } else {
+                if(index % 2 == 0){
+                    answer += Character.toString(Character.toUpperCase(c[i]));
+                } else {
+                    answer += Character.toString(Character.toLowerCase(c[i]));
+                }
+                index++;
+            }
+        }
+        
+        return answer;
+    }
+}
